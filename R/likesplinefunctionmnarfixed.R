7ecd254d582cf97dd0d6d9c1cf751a559c674d4c
Benjamin Langworthy <langw019@ad.ahc.umn.edu>
2024-02-16 12:47:22 -0600

    update to function

--- R/likesplinefunctionmnarfixed.R
+++ R/likesplinefunctionmnarfixed.R
@@ -1,7 +1,7 @@
 #'Output the MNAR MPLE likelihood
 #'
 #'#'This takes dat for competing risks survival times, where failure type is missing for some observations
-#'@param dat A data table that has observed time (failure or censoring), values of covariates in the missingness model, values of covariates in the Cox model, basis variables for the baseline hazard ratio spline model, a censoring indicator names 'cens' that takes value 0 if censored, and a cause variable named 'cause' that has a numeric value indicating the cause and takes 0 if cause is unknown 
+#'@param dat A data table that has observed time (failure or censoring), values of covariates in the missingness model, values of covariates in the Cox model, basis variables for the baseline hazard ratio spline model, a censoring indicator names 'cens' that takes value 0 if censored, and a cause variable named 'cause' that has a numeric value indicating the cause and takes 0 if cause is unknown
 #'@param params The starting values for the missingness model and Cox model coefficients associated with the covariates in the dat data frame. Missingness model currently assumed to be logistic.
 #'@param mnarparams The fixed values for the effect of missingness type on probability of missingness in a logistic model. This controls whether data are assumed to be MNAR
 #'@param timevar The name of the time variable
@@ -22,12 +22,12 @@ likesplinefunctionmnarfixed <- function(dat,params,mnarparams,timevar,xvarsmiss,
   for(i in 1:ncause){
     assign(paste0("b",i),params[grep(paste0("b",i),names(params))])
   }
-  ###parameters for hazard ratio
+  ###parameters for baseline hazard ratio
   for(i in 2:ncause){
-    assign(paste0("e",i),params[grep(paste0("e",i),names(params))])
+    assign(paste0("bas",i),params[grep(paste0("bas",i),names(params))])
   }
   b <- do.call(cbind,mget(paste0("b",1:ncause)))
-  e <- do.call(cbind,mget(paste0("e",2:ncause)))
+  bas <- do.call(cbind,mget(paste0("bas",2:ncause)))
   ###Cox portion of model
   coxbeta <- exp(tcrossprod(as.matrix(dat[,..xvarscox]),t(b)))
   ###Cumulative sum of cox model for portion for denominator
@@ -35,7 +35,7 @@ likesplinefunctionmnarfixed <- function(dat,params,mnarparams,timevar,xvarsmiss,
   colnames(coxbeta) <- paste0("beta",1:ncause)
   colnames(coxbetasum) <- paste0("betasum",1:ncause)
   ###Baseline hazard ratio
-  basrat <- cbind(tcrossprod(as.matrix(dat[,..splinevars]),t(e)))
+  basrat <- cbind(tcrossprod(as.matrix(dat[,..splinevars]),t(bas)))
   colnames(basrat) <- paste0("eta",2:ncause)
   ###Missingness model
   obsx <- tcrossprod(as.matrix(dat[,..xvarsmiss]),t(ax))
@@ -44,7 +44,7 @@ likesplinefunctionmnarfixed <- function(dat,params,mnarparams,timevar,xvarsmiss,
   dat <- data.table(dat,coxbeta,coxbetasum,basrat,pobs)
   ###Denominator of likelihood
   dat[,denom :=  eval(parse(text=paste0("betasum1+", paste(paste0("betasum",2:ncause),paste0("eta",2:ncause),sep="*",collapse = "+"))))]
-  
+
   if(ties == TRUE){
     dat[,denom:= max(denom),by=timevar]
   }
@@ -53,7 +53,7 @@ likesplinefunctionmnarfixed <- function(dat,params,mnarparams,timevar,xvarsmiss,
                                      "+(1-pobs1)*beta1*cause_0+",paste(paste0("(1-pobs",2:ncause,")"),paste0("eta",2:ncause),paste0("beta",2:ncause),paste0("cause_0"),sep="*",collapse = "+"))))]
   ###Log likelihood
   dat[,llik := log((num/denom)*cens + 1*(1-cens))]
-  
+
   return(-1*sum(dat$llik))
-  
+
 }
